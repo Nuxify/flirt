cdab244f65b53faf9e57065383b0cc25999f8375
Ralph Tolipas <tolipasralphryan10@gmail.com>
2021-03-01 23:44:31 +0800

    fix: clear text form on reset

--- lib/interfaces/screens/generate_screen.dart
+++ lib/interfaces/screens/generate_screen.dart
@@ -13,27 +13,35 @@ class GenerateScreen extends StatefulWidget {
   }) : super(key: key);
 
   static const String routeName = '/generate';
+
   @override
   _GenerateScreenState createState() => _GenerateScreenState();
 }
 
 class _GenerateScreenState extends State<GenerateScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  final String _headerTitle = 'Generate a QR code';
-  String _idTextField = '';
-  String _dataTextField = '';
   double _qrOpacity = 0.0;
+  String _generatedData = '';
+
+  /// Textbox controller
+  final TextEditingController _idTextController = TextEditingController();
+  final TextEditingController _dataTextController = TextEditingController();
 
   void _resetFields() {
     setState(() {
-      _idTextField = '';
-      _dataTextField = '';
+      _idTextController.clear();
+      _dataTextController.clear();
+      _generatedData = '';
       _qrOpacity = 0.0;
     });
+  }
+
+  void _generateQRCode(BuildContext context) {
+    final RecordRequestDTO payload = RecordRequestDTO(
+      id: _idTextController.text,
+      data: _dataTextController.text,
+    );
+    context.read<RecordCubit>().recordData(payload);
+    Scaffold.of(context).hideCurrentSnackBar();
   }
 
   @override
@@ -42,7 +50,7 @@ class _GenerateScreenState extends State<GenerateScreen> {
     final ThemeData _theme = Theme.of(context);
 
     return Scaffold(
-      appBar: Header(title: _headerTitle),
+      appBar: const Header(title: 'Generate a QR code'),
       body: SingleChildScrollView(
         child: Container(
           margin: EdgeInsets.only(
@@ -55,21 +63,20 @@ class _GenerateScreenState extends State<GenerateScreen> {
               Container(
                 margin: const EdgeInsets.only(bottom: 20),
                 child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'ID (optional)',
-                  ),
-                  onChanged: (String value) => _idTextField = value,
-                ),
+                    controller: _idTextController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'ID (optional)',
+                    )),
               ),
               Container(
                 margin: const EdgeInsets.only(bottom: 20),
                 child: TextField(
+                  controller: _dataTextController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Data',
                   ),
-                  onChanged: (String value) => _dataTextField = value,
                 ),
               ),
               Container(
@@ -78,64 +85,69 @@ class _GenerateScreenState extends State<GenerateScreen> {
                   borderRadius: BorderRadius.circular(5.0),
                 ),
                 child: BlocConsumer<RecordCubit, RecordState>(
-                  listener: (BuildContext context, RecordState state) {
-                    if (state is RecordLoading) {
-                      // disable button
-                    }
-                    if (state is RecordFailed) {
-                      Scaffold.of(context).showSnackBar(SnackBar(
-                        backgroundColor: Colors.red,
-                        content: Text('Error: ${state.message}'),
-                      ));
-                      setState(() {
-                        _qrOpacity = 0.0;
-                      });
-                    }
-                    if (state is RecordSuccess) {
-                      setState(() {
-                        _qrOpacity = 1.0;
-                      });
-                      Scaffold.of(context).showSnackBar(SnackBar(
-                        duration: const Duration(seconds: 120),
-                        backgroundColor: Colors.green,
-                        content: const Text('QR code is ready.'),
-                        action: SnackBarAction(
-                          label: 'CREATE NEW',
-                          textColor: Colors.black,
-                          onPressed: () {
-                            _resetFields();
-                          },
-                        ),
-                      ));
-                    }
-                  },
-                  builder: (BuildContext context, RecordState state) {
-                    return FlatButton.icon(
-                      icon: const Icon(Icons.select_all, color: Colors.white),
-                      label: Text('GENERATE',
-                          style: _theme.textTheme.bodyText1
-                              .copyWith(fontSize: 20)),
-                      onPressed: () {
-                        final RecordRequestDTO payload = RecordRequestDTO(
-                            id: _idTextField, data: _dataTextField);
-                        context.read<RecordCubit>().recordData(payload);
-                      },
-                      padding: const EdgeInsets.only(
-                          top: 10, bottom: 10, left: 20, right: 20),
-                      color: Theme.of(context).primaryColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0)),
-                    );
-                  },
-                ),
+                    listener: (BuildContext context, RecordState state) {
+                  if (state is RecordLoading) {
+                    // disable button
+                  }
+                  if (state is RecordFailed) {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      backgroundColor: _theme.errorColor,
+                      content: Text('Error: ${state.message}'),
+                    ));
+                    setState(() {
+                      _qrOpacity = 0.0;
+                    });
+                  }
+                  if (state is RecordSuccess) {
+                    setState(() {
+                      _qrOpacity = 1.0;
+                      _generatedData = state.recordResponse.data;
+                    });
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      duration: const Duration(seconds: 120),
+                      backgroundColor: Colors.green,
+                      content: const Text('QR code is ready.'),
+                      action: SnackBarAction(
+                        label: 'CREATE NEW',
+                        textColor: Colors.black,
+                        onPressed: () => _resetFields(),
+                      ),
+                    ));
+                  }
+                }, builder: (BuildContext context, RecordState state) {
+                  return FlatButton.icon(
+                    icon: const Icon(
+                      Icons.select_all,
+                      color: Colors.white,
+                    ),
+                    label: Text('GENERATE',
+                        style: _theme.textTheme.bodyText1.copyWith(
+                          fontSize: 20,
+                        )),
+                    onPressed: () => _generateQRCode(context),
+                    color: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0)),
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 10,
+                      left: 20,
+                      right: 20,
+                    ),
+                  );
+                }),
               ),
               Container(
-                margin: EdgeInsets.only(top: _mediaQuery.size.height * 0.08),
+                margin: EdgeInsets.only(
+                  top: _mediaQuery.size.height * 0.08,
+                ),
                 child: AnimatedOpacity(
                   opacity: _qrOpacity,
-                  duration: const Duration(seconds: 1),
+                  duration: const Duration(
+                    seconds: 1,
+                  ),
                   child: QrImage(
-                    data: _idTextField,
+                    data: _generatedData,
                     version: QrVersions.auto,
                     size: 200.0,
                   ),
