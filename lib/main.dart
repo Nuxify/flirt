8d06b5660c23ef8298f64408282fda1303e6f5bc
Gabriel <josesgabriellu@gmail.com>
2022-10-07 11:23:47 +0800

    chore: remove /routes folder as Named Routes are no longer the recommended way of navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flirt/configs/themes.dart';
 import 'package:flirt/module/home/interfaces/screens/home_screen.dart';
 import 'package:flirt/module/quote/service/cubit/quote_cubit.dart';
-import 'package:flirt/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -26,7 +25,6 @@ class App extends StatelessWidget {
         supportedLocales: const <Locale>[
           Locale('en'),
         ],
-        routes: routes,
         debugShowCheckedModeBanner: false,
       ),
     );
--- lib/routes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flirt/module/home/interfaces/screens/home_screen.dart';
-import 'package:flutter/material.dart';
-
-Map<String, Widget Function(BuildContext)> routes =
-    <String, Widget Function(BuildContext)>{
-  HomeScreen.routeName: (BuildContext ctx) => const HomeScreen(),
-};
