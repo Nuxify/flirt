1f771bbf19e83d794c22341c573fdd0d35ec6bd8
josephgcedeno <josephgcedeno@gmail.com>
2022-10-19 12:38:19 +0800

    feat: access the value inside stateless widget and using watch to update list

---
+++ lib/module/home/interfaces/screens/list_quotes.dart
@@ -0,0 +1,61 @@
+import 'package:flirt/configs/themes.dart';
+import 'package:flirt/module/quote/service/cubit/quote_cubit.dart';
+import 'package:flirt/module/quote/service/cubit/quote_dto.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ListQuotes extends StatelessWidget {
+  const ListQuotes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<QuoteResponseDTO> state =
+        context.watch<QuoteCubit>().state.state as List<QuoteResponseDTO>;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of quotes'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: flirtGradient,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                    'List is updated when the state is updated',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: state.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(state[index].en),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
