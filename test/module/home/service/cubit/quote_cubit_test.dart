67fc7fdaf81c97959116ecc21526d6adc1eb0b4c
josephgcedeno <josephgcedeno@gmail.com>
2022-11-15 18:33:04 +0800

    chore: create a test on what should happen in quote cubit eg. fetch quote on success should emit  state that is FetchQuoteSuccess

---
+++ test/module/home/service/cubit/quote_cubit_test.dart
@@ -0,0 +1,55 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flirt/infrastructures/models/api_response.dart';
+import 'package:flirt/infrastructures/models/quote/quote_response.dart';
+import 'package:flirt/infrastructures/repository/quote_repository.dart';
+import 'package:flirt/module/home/service/cubit/quote_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockQuoteRepository extends Mock implements QuoteRepository {}
+
+void main() {
+  late MockQuoteRepository mockQuoteRepository;
+
+  setUp(() {
+    mockQuoteRepository = MockQuoteRepository();
+  });
+  group('Quote cubit.', () {
+    blocTest<QuoteCubit, QuoteState>(
+      'On successful fetch quote, it should emit FetchQuoteSuccess.',
+      build: () {
+        when(() => mockQuoteRepository.fetchQuote()).thenAnswer((_) async {
+          return QuoteResponse(author: '', en: '', id: '');
+        });
+
+        return QuoteCubit(quoteRepository: mockQuoteRepository);
+      },
+      act: (QuoteCubit cubit) => cubit.fetchQuote(),
+      expect: () => <TypeMatcher<QuoteState>>[
+        isA<FetchQuoteLoading>(),
+        isA<FetchQuoteSuccess>(),
+      ],
+    );
+
+    blocTest<QuoteCubit, QuoteState>(
+      'On failed fetch quote, it should emit FetchQuoteFailed.',
+      build: () {
+        when(() => mockQuoteRepository.fetchQuote()).thenThrow(
+          APIResponse<QuoteResponse>(
+            message: '',
+            success: false,
+            errorCode: '',
+            data: QuoteResponse(author: '', en: '', id: ''),
+          ),
+        );
+
+        return QuoteCubit(quoteRepository: mockQuoteRepository);
+      },
+      act: (QuoteCubit cubit) => cubit.fetchQuote(),
+      expect: () => <TypeMatcher<QuoteState>>[
+        isA<FetchQuoteLoading>(),
+        isA<FetchQuoteFailed>(),
+      ],
+    );
+  });
+}
