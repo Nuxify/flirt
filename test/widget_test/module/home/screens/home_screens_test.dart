437b93337ea75a5859dc779b8052dda82e2f876d
josephgcedeno <josephgcedeno@gmail.com>
2022-11-18 16:38:45 +0800

    fix: test case stub

--- test/widget_test/module/home/screens/home_screens_test.dart
+++ test/widget_test/module/home/screens/home_screens_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flirt/module/home/interfaces/screens/home_screen.dart';
 import 'package:flirt/module/home/service/cubit/quote_cubit.dart';
+import 'package:flirt/module/home/service/cubit/quote_dto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -25,7 +26,11 @@ void main() {
         ),
       );
   void listenStub() {
-    when(() => mockQuoteCubit.state).thenReturn(QuoteInitial());
+    when(() => mockQuoteCubit.state).thenReturn(
+      QuoteState(
+        data: QuoteStateDTO(authors: <String>[], quotes: <QuoteResponseDTO>[]),
+      ),
+    );
     when(() => mockQuoteCubit.fetchQuote()).thenAnswer((_) async {});
   }
 
