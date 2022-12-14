c59ba4d17794f7ed598a5ae692d5a320932de267
josephgcedeno <josephgcedeno@gmail.com>
2022-12-14 17:37:13 +0800

    chore: initialize dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:flirt/module/home/interfaces/screens/home_screen.dart';
 import 'package:flirt/module/home/service/cubit/quote_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(const App());
+void main() async {
+  /// Load env file
+  await dotenv.load();
+
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
