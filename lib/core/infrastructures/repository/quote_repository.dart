92ad42482007de4648468a9f251fff767c10617c
josephgcedeno <josephgcedeno@gmail.com>
2024-10-24 10:36:32 +0800

    chore: enable getter

--- lib/core/infrastructures/repository/quote_repository.dart
+++ lib/core/infrastructures/repository/quote_repository.dart
@@ -1,24 +1,24 @@
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:flirt/core/domain/models/api_error_response.dart';
 import 'package:flirt/core/domain/models/api_response.dart';
 import 'package:flirt/core/domain/models/quote/quote_response.dart';
 import 'package:flirt/core/domain/repository/quote_repository.dart';
+import 'package:flirt/core/domain/repository/secure_storage_repository.dart';
+import 'package:flirt/core/infrastructures/repository/secure_storage_repository.dart';
+import 'package:flirt/internal/env.dart';
 import 'package:flirt/internal/utils.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class QuoteRepository implements IQuoteRepository {
-  // final ISecureStorageRepository _storage = SecureStorageRepository();
+  final ISecureStorageRepository _storage = SecureStorageRepository();
 
-  final String _baseURL = dotenv.get('API_ENV') != 'production'
-      ? dotenv.get('QUOTE_API')
-      : dotenv.get('STAGING_QUOTE_API');
   final String _repositoryURL = '/api/v1/flirt/quote/random';
 
   /// This getter can be used to set the base URL based on the environment,
   /// if you are running a staging/production environment
-  // Future<String> get _baseURL => checkEnvironment(_storage);
+  Future<String> get _baseURL => checkEnvironment(_storage);
 
   @override
   Future<APIListResponse<QuoteResponse>> fetchQuote() async {
@@ -26,7 +26,7 @@ class QuoteRepository implements IQuoteRepository {
 
     try {
       response = await http.get(
-        Uri.https(_baseURL, _repositoryURL),
+        Uri.https(await _baseURL, _repositoryURL),
         headers: httpRequestHeaders,
       );
 
