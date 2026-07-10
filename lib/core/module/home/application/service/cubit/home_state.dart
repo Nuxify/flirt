8aba0a93babb2473b7c67931efbb83bae24dc451
Joseph Cedeño <josephgcedeno@gmail.com>
2026-07-10 14:01:47 +0800

    feat(core): add fetchQuoteOnce method and related state classes for one-off quote requests

--- lib/core/module/home/application/service/cubit/home_state.dart
+++ lib/core/module/home/application/service/cubit/home_state.dart
@@ -28,3 +28,19 @@ class FetchQuoteFailed extends HomeState {
   final String errorCode;
   final String message;
 }
+
+class FetchQuoteOnceLoading extends HomeState {
+  FetchQuoteOnceLoading({required super.data});
+}
+
+class FetchQuoteOnceSuccess extends HomeState {
+  FetchQuoteOnceSuccess(this.quoteResponse, {required super.data});
+
+  final QuoteResponseDTO quoteResponse;
+}
+
+class FetchQuoteOnceFailed extends HomeState {
+  FetchQuoteOnceFailed({required this.message, required super.data});
+
+  final String message;
+}
